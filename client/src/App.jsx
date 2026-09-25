963399efeaa32fd5757b029a3ed5171815ae199d
Arvind <msarvindm@gmail.com>
2026-09-25 11:34:23 +0530

    changed link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+const API_URL = import.meta.env.VITE_API_URL || 'https://studentform-i3xs.onrender.com'
 
 const companies = [
   ['TCS', 'Information technology', 'blue'], ['Wipro', 'Information technology', 'violet'], ['HCLTech', 'Technology services', 'orange'],
